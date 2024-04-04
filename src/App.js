e9af72110303505e29f364ade5c0b18e86c4d10f
Ragavan67 <150412969+Ragavan67@users.noreply.github.com>
2024-04-04 14:08:10 +0530

    ef

---
+++ src/App.js
@@ -0,0 +1,44 @@
+
+import './App.css';
+import World from  './World';
+
+function App() {
+ 
+  return (
+
+  <>
+ 
+   
+  <div class='row'>
+    <div className='col-md-12 col-sm-12'>
+      <div className='App-header'>
+        <div className='app-div'>
+          <button className='btn btn-danger' id='btn1'>anouncemnet</button>
+          <div className='head-content '>
+            <p>How Were Responding Covid-19</p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    </div>
+  
+    
+     {/* NavBar */}
+     
+     <World />
+      
+     </>
+
+     
+   
+
+
+
+
+
+    
+  );
+}
+
+export default App;
---
+++ src/World.js
@@ -0,0 +1,18 @@
+function World(){
+    return(
+        <>
+        <div className="container-fluid mt-5">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a href="#" className="nav-brand">
+                Ragavan
+            </a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ragavan">
+                <span className="navbar-toggler-icon"></span>
+                
+                </button>  
+        </nav>
+        </div>
+        </>
+    );
+}
+export default World;
